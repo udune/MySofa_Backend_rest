36eee313af09550c31b8d1722212b4501f8f7c52
udune <udune@naver.com>
2025-07-02 03:29:01 +0900

    전역 에러 배열 메시지 처리 누락 수정

--- src/common/filters/all-exceptions.filter.ts
+++ src/common/filters/all-exceptions.filter.ts
@@ -4,11 +4,14 @@ import {
   ExceptionFilter,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { Response } from 'express';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllExceptionsFilter.name);
+
   catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
@@ -22,7 +25,13 @@ export class AllExceptionsFilter implements ExceptionFilter {
       message =
         typeof exceptionResponse === 'string'
           ? exceptionResponse
-          : (exceptionResponse as any).message || message;
+          : Array.isArray((exceptionResponse as any).message)
+            ? (exceptionResponse as any).message.join(', ')
+            : (exceptionResponse as any).message || message;
+    }
+
+    if (status === HttpStatus.INTERNAL_SERVER_ERROR) {
+      this.logger.error('Internal Server Error', exception);
     }
 
     response.status(status).json({
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/require-await */
 import {
-  BadRequestException,
   Body,
   Controller,
   HttpCode,
